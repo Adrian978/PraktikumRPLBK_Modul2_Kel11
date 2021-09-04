9ea0b4532514f54a6f1c1f8fb819bbb9baf3d8a5
Adrian978 <49022717+Adrian978@users.noreply.github.com>
2021-09-04 10:37:35 +0700

    Commit Pertama

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import CardList from "./components/CardList";
+import Quotes from "./components/Quotes";
+
+import Gambar from "./assets/images/gambar1.jpg";
+
+const quotes = "Secapek-capeknya kerja lebih capek nganggur.";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <CardList
+      name="adriansyah"
+      nim="21120118130098"
+      kelompok="11"
+      isNameBold
+      image={Gambar}
+    />
+    <Quotes author="Ernest Prakasa" quotes={quotes} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
